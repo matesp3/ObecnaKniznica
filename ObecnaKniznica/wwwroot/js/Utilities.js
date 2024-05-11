7714ebbcd42258225714b2c0ad2db90b01783c65
Matej Poljak <mpoljak3@gmail.com>
2024-05-11 23:46:52 +0200

    Layout Done

--- ObecnaKniznica/wwwroot/js/Utilities.js
+++ ObecnaKniznica/wwwroot/js/Utilities.js
@@ -1,12 +1,4 @@
-﻿//deleteBtn.addEventListener('click', (event) => {
-//    addBottomSpace(event);
-//});
-
-//deleteBtn.addEventListener('click', (event) => {
-//    deleteBottomSpace(event);
-//});
-
-function addBottomSpace() {
+﻿function toggleBlankSpace() {
     const divAdditionalBottom = document.getElementById('addedBottomSpace');
     if (divAdditional) {
         divAdditionalBottom.parentNode.removeChild(divAdditionalBottom); // vymazanie - navbar sa zavrel
@@ -27,37 +19,20 @@ function addBottomSpace() {
                 // priprav element s prazdnym miestom
                 const blankSpaceDiv = document.createElement('div');
                 blankSpaceDiv.style.height = count * 5; // 5px * count
-                blankSpaceDiv.className = "container-fluid";
+                blankSpaceDiv.className = "row";
                 blankSpaceDiv.id = "addedBottomSpace";
+                blankSpaceDiv.style.color = "red";
                 // najdi miesto, kde element zaradit
+                document.getElementById('web-content').appendChild(blankSpaceDiv);
             }
         }
-    }
-
-
-    var divContent = document.getElementById('navbarClickedItem');
-    var count = divContent.getChilds.count;
-    var newDiv = Ddocument.createElement('div');
-    newDiv.id = "addedBottomSpace;
-    newDiv.style.width = 10 * count; // 10px * pocet vnorenych elementov
-}
-
-function deleteBottomSpace() {
-    var div = document.getElementById('addedBottomSpace');
-    if (div != null) {
-        var parentDiv = div.getParent();
-        parentDiv.delete(div);
     }
 }
 
 window.onload = (event) => {
     const toggleBtn = document.getElementById('hamburgerBtn');
     toggleBtn.addEventListener('click', (event) => {
-        addBottomSpace();
-    });
-
-    toggleBtn.addEventListener('click', (event) => {
-        deleteBottomSpace();
+        toggleBlankSpace();
     });
 
     const tableHours = document.getElementById('tableHours');
