b73c111c82f9358c47b50da69b7ac4459439d344
Matej Poljak <mpoljak3@gmail.com>
2024-05-14 13:28:09 +0200

    JSInteroperability for WorkingHours + About site

---
+++ ObecnaKniznica/Pages/Catalogue/BookAuthor.cs
@@ -0,0 +1,9 @@
+﻿namespace ObecnaKniznica.Pages.Catalogue
+{
+    public class BookAuthor
+    {
+        public string? id { get; set; } = null;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
--- ObecnaKniznica/wwwroot/js/Utilities.js
+++ ObecnaKniznica/wwwroot/js/Utilities.js
@@ -1,61 +1,28 @@
-﻿function toggleBlankSpace() {
-    const divAdditionalBottom = document.getElementById('addedBottomSpace');
-    if (divAdditional) {
-        divAdditionalBottom.parentNode.removeChild(divAdditionalBottom); // vymazanie - navbar sa zavrel
-    }
-    else { // neexistuje 
-        const navbarItems = document.getElementsByClassName('itemOfNavbar');
-        if (navbarItems) {
-            if (navbarItems.length > 0) {
-                let count = 0;
-                for (let i = 0; i < navbarItems.length; i++) {
-                    let item = navbarItems[i];
-                    if (item.className.includes("dropdown-toggle")) {
-                        count += item.nextSibling.childNodes.length // child nodes elementu "ul"
-                    } else {
-                        count++;
-                    }
-                }
-                // priprav element s prazdnym miestom
-                const blankSpaceDiv = document.createElement('div');
-                blankSpaceDiv.style.height = count * 5; // 5px * count
-                blankSpaceDiv.className = "row";
-                blankSpaceDiv.id = "addedBottomSpace";
-                blankSpaceDiv.style.color = "red";
-                // najdi miesto, kde element zaradit
-                document.getElementById('web-content').appendChild(blankSpaceDiv);
-            }
-        }
-    }
-}
-
-window.onload = (event) => {
-    const toggleBtn = document.getElementById('hamburgerBtn');
-    toggleBtn.addEventListener('click', (event) => {
-        toggleBlankSpace();
-    });
-
+﻿window.updateWorkingHours = () => {
     const tableHours = document.getElementById('tableHours');
     let dayPosition = new Date().getDay();
     if (tableHours != null && dayPosition !== 0) {
-        //const tbody = tableHours.childNodes.item(0).childNodes;
         const editClassName = "highlighted";
         const normalClassName = "";
-        tableHours.firstChild.firstChild.className = "highlighted";
-        //let tableRow = tableHours.firstChild.firstChild;
-        //tableRow.firstChild.className = "highlighted";
-        //let i = 1;
-        //let tableRow = tableHours.firstChild.firstChild;
-        //while (tableRow) {
-        //    const newClassName = (i === dayPosition) ? editClassName : normalClassName;
-        //    tableRow.firstChild.className = newClassName;
-        //    tableRow.lastChild.className = newClassName;
-        //    i++;
-        //    tableRow = tableRow.nextSibling;
-        //}
-/*        tbody.item(2 * (dayPosition - 1));*/
-
-        //tbody.item(2 * (dayPosition - 1)).style.fontWeight = 'bold'; //2,4,6
+        
+        let tableRow = tableHours.firstChild.firstChild;
+        let i = 1;
+        while (tableRow) {
+            if (tableRow.nodeName === "TR") { // strieda sa s textom
+                const newClassName = (i === dayPosition) ? editClassName : normalClassName;
+                tableRow.firstChild.className = newClassName;
+                tableRow.lastChild.className = newClassName;
+                i++;
+            }
+            tableRow = tableRow.nextSibling;
+        }
     }
-}
+};
+
+//window.onload = (event) => {
+//    const toggleBtn = document.getElementById('hamburgerBtn');
+//    toggleBtn.addEventListener('click', (event) => {
+//        toggleBlankSpace();
+//    });
+//}
 
