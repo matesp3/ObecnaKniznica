c57b35012ea1064e7c8a8459eb53733832ae621d
Matej Poljak <mpoljak3@gmail.com>
2024-05-17 10:57:18 +0200

    edit form updated

--- ObecnaKniznicaLogic/DataModels/BookModel.cs
+++ ObecnaKniznicaLogic/DataModels/BookModel.cs
@@ -1,19 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using ObecnaKniznicaLogic.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace ObecnaKniznicaLogic.DataModels
 {
     public class BookModel
     {
-        public int Id { get; set; }
         [Required]
-        public required string Title { get; set; }
+        public string? Title { get; set; }
         public string? Image { get; set; }
-        [Required]
-        public int TotalAmount { get; set; }
-        public int ReservedAmount { get; set; }
+ 
+        public int? AvailableAmount { get; set; }
         [Required]
         public string? Description { get; set; }
         public DateTime? ReleaseDate { get; set; }
-        public DateTime Created { get; set; } = DateTime.Now; // in system
+
+        public List<Author>? Authors { get; set; }
     }
 }
