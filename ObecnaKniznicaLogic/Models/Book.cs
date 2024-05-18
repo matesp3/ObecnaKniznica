aca581be0dbacfbca28a2741cfb8bcbc2478900d
Matej Poljak <mpoljak3@gmail.com>
2024-05-18 12:15:21 +0200

    BookForm validation -added

--- ObecnaKniznicaLogic/Models/Book.cs
+++ ObecnaKniznicaLogic/Models/Book.cs
@@ -5,11 +5,11 @@ namespace ObecnaKniznicaLogic.Models
     public class Book
     {
         public int Id { get; set; } = 0;
-        [Required]
-        [MinLength(2)]
+        [Required(ErrorMessage = "Titul knihy je povinný!")]
+        [MinLength(2, ErrorMessage = "Titul knihy musí obsahovať aspoň 2 znaky!")]
         public string Title { get; set; } = String.Empty;
-        [Required]
-        [Range(1, Double.MaxValue, ErrorMessage = "Total amount must be greater than 0.")]
+        [Required(ErrorMessage = "Celkové množstvo kníh je povinné!")]
+        [Range(1, Double.MaxValue, ErrorMessage = "Celkové množstvo musí byť viac ako 0!")]
         public int TotalAmount { get; set; } = 0;
         [Required]
         public int ReservedAmount { get; set; } = 0;
